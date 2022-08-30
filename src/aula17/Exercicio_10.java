3ac0ff6cb2201ef663b6b5af88174214b6570906
evandro <evandroamado@gmail.com>
2022-08-30 19:03:42 -0300

    adding exercise 10

---
+++ src/aula17/Exercicio_10.java
@@ -0,0 +1,22 @@
+package aula17;
+
+/*Faça um programa que imprima na tela apenas os números ímpares
+entre 1 e 50.*/
+
+public class Exercicio_10 {
+
+	public static void main(String[] args) {
+		int[] numeros = new int[100]; 
+		int divisao;
+		int i = 0;
+		
+		for(; i < 100; i++) {
+			if(i < 50) {
+				divisao = i % 2;
+				if(divisao == 1) {
+					System.out.println(i);
+				}
+			}
+		}System.out.println("Total de  números foi " + i);
+	}
+}
