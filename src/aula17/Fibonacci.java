9bbaf4f2a4928f45f10b264d196e637b6aed26d0
evandro <evandroamado@gmail.com>
2022-09-01 09:52:33 -0300

    adding exercise fibonacci

---
+++ src/aula17/Fibonacci.java
@@ -0,0 +1,12 @@
+package aula17;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		for(int i = 0; i <= 20; i++) {
+			System.out.print(FormulaFibonacci.fibonacci(i) + " ");
+		}
+
+	}
+
+}
---
+++ src/aula17/FormulaFibonacci.java
@@ -0,0 +1,10 @@
+package aula17;
+
+public class FormulaFibonacci {
+	public static int fibonacci(int numero) {
+		if(numero < 2) {
+			return 1;
+		}
+		return fibonacci(numero - 1) + fibonacci(numero - 2);
+	}
+}
