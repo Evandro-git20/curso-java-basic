4d1fe67ebb196c06a0b3ebb755be8dcfe7e36eda
evandro <evandroamado@gmail.com>
2022-09-06 16:39:14 -0300

    adicionando OitavoExercicio

---
+++ src/arrays/oitavoExercicio.java
@@ -0,0 +1,33 @@
+package arrays;
+
+/*Criar dois vetores A e B cada um com 10 elementos inteiros. Construir
+um vetor C, onde cada elemento de C é a multiplicação dos
+respectivos elementos em A e B, ou seja:
+C[i] = A[i] * B[i].*/
+
+public class oitavoExercicio {
+
+	public static void main(String[] args) {
+		int[] vetorA = new int[10];
+		int[] vetorB = new int[vetorA.length];
+		int[] vetorC = new int[vetorB.length];
+		
+		for(int i = 0; i < vetorA.length; i++) {
+			vetorA[i] = i;
+			vetorB[i] = vetorA[i];
+			vetorC[i] = vetorA[i] * vetorB[i];
+		}
+		System.out.print("Vetor A: ");
+		for(int i = 0; i < vetorA.length; i++) {
+			System.out.print(vetorA[i]+" ");
+		}
+		System.out.print("\nVetor B: ");
+		for(int i = 0; i < vetorB.length; i++) {
+			System.out.print(vetorB[i]+" ");
+		}
+		System.out.print("\nVetor C: ");
+		for(int i =0; i < vetorC.length; i++) {
+			System.out.print(vetorC[i]+" ");
+		}
+	}
+}
