9b66e885358d1e33d5aa47da581417d83ff03853
evandro <evandroamado@gmail.com>
2022-09-06 16:47:40 -0300

    adicionando DecimoExercicio

---
+++ src/arrays/DecimoExercicio.java
@@ -0,0 +1,28 @@
+package arrays;
+
+/*Criar um vetor A com 10 elementos inteiros. Construir um vetor B de
+mesmo tipo e tamanho, sendo que cada elemento do vetor B deverá
+ser o resto da divisão do respectivo elemento de A por 2 (dois), ou
+seja: B[i] := A[i] % 2.*/
+
+public class DecimoExercicio {
+
+	public static void main(String[] args) {
+		int[] vetorA = new int[10];
+		int[] vetorB = new int[vetorA.length];
+		int[] vetorC = new int[vetorB.length];
+		
+		for(int i = 1; i < vetorA.length; i++) {
+			vetorA[i] = i;
+			vetorB[i] = vetorA[i] % 2;
+		}
+		System.out.print("Vetor A: ");
+		for(int i = 0; i < vetorA.length; i++) {
+			System.out.print(vetorA[i]+" ");
+		}
+		System.out.print("\nVetor B: ");
+		for(int i = 0; i < vetorB.length; i++) {
+			System.out.print(vetorB[i]+" ");
+		}
+	}
+}
