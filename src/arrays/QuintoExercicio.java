4baaa2487a6862b471ab279e0660405fcbc7a12c
evandro <evandroamado@gmail.com>
2022-09-06 20:23:41 -0300

    adicionando QuintoExercicio

--- src/arrays/QuintoExercicio.java
+++ src/arrays/QuintoExercicio.java
@@ -1,7 +1,5 @@
 package arrays;
 
-import java.text.DecimalFormat;
-
 /*Criar um vetor A com 10 elementos inteiros. Construir um vetor B de
 mesmo tipo e tamanho, sendo que cada elemento do vetor B deverá
 ser o respectivo elemento de A multiplicado por sua posição (ou
